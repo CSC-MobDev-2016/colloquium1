2c473b34521554d9f8bd81f12d07b76ce638bc3a
Maxim <sssmaximusss@gmail.com>
2016-04-12 21:22:05 +0300

    init, design

---
+++ app/src/main/java/com.csc.smax.converter/MainActivity.java
@@ -0,0 +1,29 @@
+package com.csc.smax.converter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.update_button) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
